b8e306ee098b3fabb4e6ff9c5cac179939af53ab
ZelongLiu5167 <zl5167@nyu.edu>
2024-10-04 19:00:29 +0800

    Add files via upload

---
+++ mid/sketch.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', function () {
+    let zodiacData;
+    let input = document.getElementById('year-input');
+    let button = document.getElementById('zodiac-button');
+    let zodiacResult = document.getElementById('animal');
+    let zodiacexplanation = document.getElementById('explanation');
+  
+    button.addEventListener('click', function() {
+        let birthyear = input.value;
+  
+        fetch('./zodiac.json')
+            .then(response => response.json())
+            .then(data => {
+                zodiacData = data;
+  
+                let foundZodiac = zodiacData.zodiac.find(entry => entry.year === parseInt(birthyear));
+  
+                if (foundZodiac) {
+                    zodiacResult.innerHTML = foundZodiac.animal; 
+                    zodiacexplanation.innerHTML = foundZodiac.explanation;
+                }
+            });
+    });
+  });
+
